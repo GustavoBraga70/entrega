c45811f01ea52c2965f42c22cd8ece8bbb202bdc
Gustavo Braga <gustavobraga236@gmail.com>
2025-05-10 20:15:51 -0300

    Update parouimpar01.js
    if ternario

--- Projetosacademicos/Parouimpar/parouimpar01.js
+++ Projetosacademicos/Parouimpar/parouimpar01.js
@@ -1,14 +1,6 @@
 const prompt = require('prompt-sync')({sigint:true});
 
-let n1 = prompt("Digite seu número: ");
-let order = parseFloat(n1 % 2);
+let n1 = prompt("Selecione seu número: ");
+let order = (n1 % 2 == 0) ? "Par" : "Ímpar";
 
-if ( order == 0 ) {
-    console.log (`O número ${n1} é PAR!`);
-}
-else if ( order == 1 ) {
-    console.log(`O número ${n1} é ÍMPAR!`);
-}
-else {
-    console.log(`O número ${n1} NÃO é PAR nem ÍMPAR!`);
-}
+console.log(`Seu número é ${order}`);
